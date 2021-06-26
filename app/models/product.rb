3d981732675f4392eba1c20a31d3bfa83087e97c
Sebastian Andres <sebastianandres@MacBook-Air-de-Sebastian.local>
2021-06-25 22:22:47 -0400

    validation_main

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,20 @@ class Product < ApplicationRecord
     after_save :send_notification
     after_save :push_notification, if: :discount? # si el metodo discount? = true, callback se ejecuta
 
+    validates :title, presence: {message: "Es necesario el titulo"} # cambiar el mensaje, presence = true default
+    validates :code, uniqueness: {message: "El code: %{value} esta en uso"} # response, %{valor del atributo}
+    
+    # validates :price, length: {minimum:3, maximum:5} 
+    #Validate with condional same before
+    validates :price, length: {in: 3..5, message:"El precio esta fuera de rango"}, if: :has_price?
+    # si el methodo has_price return true, se ejecuta la validacion, en caso contrario se ignora
+
+
+    #used validation
+    # var with valid?
+    # var with persisted?
+    # var with error.messages
+
     def total
         self.price / 100
     end
@@ -12,9 +26,13 @@ class Product < ApplicationRecord
     def discount?
         self.total < 5
     end
+    #example if product free
+    def has_price?
+        !self.price.nil? && self.price > 0 # si el producto tiene un precio y es mayor a 0 (response: true or false)
+    end
 
     private
-    
+
     def validate_product
         puts "\n\n>>> Un nuevo producto sera añdidp a almacen!"
     end
