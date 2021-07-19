328c1a3c32d790a58e59f639cadbc5ef49e3cbbf
Sebastian Andres <sebastianandres@MacBook-Air-de-Sebastian.local>
2021-07-18 22:05:30 -0400

    change db to PG and model: shopping_cart created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_010201) do
+ActiveRecord::Schema.define(version: 2021_07_19_015712) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -21,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_07_19_010201) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "shopping_carts", force: :cascade do |t|
+    t.integer "total"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email"
@@ -28,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_07_19_010201) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "shopping_carts", "users"
 end
